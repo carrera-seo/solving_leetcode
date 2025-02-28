fee0c0c3030d9acd83beb3a1eb6d115b1cebf830
Carrera Seo <wonick.seo@baligames.xyz>
2025-02-28 11:03:27 +0900

    * 20.Valid Parentheses 처음에 []rune 에 prepend 를 이용해서 했는데 느려서 그냥 apppend 로 바꿧더니 빨라졌음.

--- 20.Valid_Parentheses.go
+++ 20.Valid_Parentheses.go
@@ -1,28 +1,35 @@
 package main
 
+func popLast(charList []rune) (rune, []rune) {
+	if len(charList) == 0 {
+		return 0, charList // 빈 슬라이스 처리
+	}
+	last := charList[len(charList)-1]     // 마지막 요소
+	newList := charList[:len(charList)-1] // 마지막 요소 제외한 슬라이스
+	return last, newList
+}
+
 func isValid(s string) bool {
 
 	var charList []rune
+	var last rune
 
 	for _, char := range s {
 
 		// 순서대로 열림 문자일때는 push
 		// 닫힘 문자 일때는 push 목록에서 찾아서 제거.
 
-		if char == '(' || char == '{' || char == '[' {
+		if len(charList) == 0 && (char == ')' || char == '}' || char == ']') {
+			return false
+		} else if char == '(' || char == '{' || char == '[' {
+			//charList = append([]rune{char}, charList...)
+			charList = append(charList, char)
+			continue
+		}
 
-			charList = append([]rune{char}, charList...)
+		last, charList = popLast(charList)
 
-		} else if len(charList) > 0 && (char == ')' || char == '}' || char == ']') {
-
-			if (char == ')' && charList[0] == '(') ||
-				(char == '}' && charList[0] == '{') ||
-				(char == ']' && charList[0] == '[') {
-				charList = charList[1:]
-			} else {
-				return false
-			}
-		} else {
+		if (char == ')' && last != '(') || (char == '}' && last != '{') || (char == ']' && last != '[') {
 			return false
 		}
 	}
